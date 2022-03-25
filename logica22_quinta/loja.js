4f98bbd1d36ed85b2c00f4528c05cc28f634b524
eduardomunareto <eduardomunareto@hotmail.com>
2022-03-24 21:32:53 -0300

    Primeiro programa

---
+++ logica22_quinta/loja.js
@@ -0,0 +1,16 @@
+const prompt = require("prompt-sync")()
+
+const modelo = prompt("Modelo do Veículo: ")
+const preco = Number(prompt("Preço do Veículo: "))
+
+const entrada = preco * 0.50
+const parcela = entrada / 12
+
+
+console.log("Entrada R$: " + padraoMoeda(entrada));
+console.log("Saldo em 12x de: " + padraoMoeda(parcela));
+
+
+function padraoMoeda(m) {
+  return m.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+}
